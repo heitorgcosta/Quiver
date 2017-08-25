02a0d80d61e0d82ea491c3c111a907abac0a6e24
heitor.costa <heitor.costa@altran-ais.com.br>
2017-08-25 20:25:12 -0300

    Initial Commit

---
+++ Quiver/Quiver.h
@@ -0,0 +1,19 @@
+//
+//  Quiver.h
+//  Quiver
+//
+//  Created by Heitor Costa on 25/08/17.
+//  Copyright © 2017 Heitor Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Quiver.
+FOUNDATION_EXPORT double QuiverVersionNumber;
+
+//! Project version string for Quiver.
+FOUNDATION_EXPORT const unsigned char QuiverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Quiver/PublicHeader.h>
+
+
